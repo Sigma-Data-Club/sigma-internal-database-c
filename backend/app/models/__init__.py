8c618bdff5c9cf37a52d747c2844b7f00f4b0819
Viktor <whyjustnot@gmail.com>
2026-02-25 21:28:54 +0100

    alembic migration, to update some tables values

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -4,7 +4,7 @@ from .enums import (
 )
 
 from .academic_program import AcademicProgram
-from .member import Member
+from .academic_credit import AcademicCredit
 
 from .role import Role
 from .permission import Permission
@@ -14,18 +14,22 @@ from .member_role import MemberRole
 from .event import Event
 from .event_application import EventApplication
 from .event_feedback import EventFeedback
+from .event_sponsor import EventSponsor
 
 from .project import Project
 from .project_member import ProjectMember
-from .academic_credit import AcademicCredit
+
 
 from .finance_transaction import FinanceTransaction
 
 from .sponsor import Sponsor
-from .event_sponsor import EventSponsor
 
 from .social_platform_snapshot import SocialPlatformSnapshot
+
 from .activity_log import ActivityLog
 
-from .member_auth import MemberAuth
 from .auth_session import AuthSession
+
+from app.models.member import Member
+from app.models.member_auth import MemberAuth
+from app.models.auth_session import AuthSession
