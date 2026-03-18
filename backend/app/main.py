e9bdda4548740dd42e7d03d1696b5ab5648f6ee0
Viktor <whyjustnot@gmail.com>
2026-03-18 19:42:53 +0100

    CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,7 @@
 from contextlib import asynccontextmanager
 
+from fastapi.middleware.cors import CORSMiddleware
+
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
 from sqlalchemy import text
@@ -68,3 +70,13 @@ def create_app() -> FastAPI:
 
 
 app = create_app()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
